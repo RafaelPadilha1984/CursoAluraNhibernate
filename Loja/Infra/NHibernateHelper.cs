9fb709235a62fe3ba4a6d86b4cbb495d3fda7235
Rafael Padilha <rafa.bcc@gmail.com>
2018-08-23 22:44:41 -0300

    Versionamento inicial
    Projeto contendo a classe usuário já mapeada e configurada. O programa de teste já cria a tabela usuário no sql server

---
+++ Loja/Infra/NHibernateHelper.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NHibernate.Tool.hbm2ddl;
+using Loja.Entidades;
+
+namespace Loja.Infra
+{
+    public class NHibernateHelper
+    {
+        public static Configuration RecuperaConfiguracao()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Usuario).Assembly);
+
+            return cfg;
+        }
+        public static void GeraSchema()
+        {
+            Configuration cfg = RecuperaConfiguracao();
+            new SchemaExport(cfg).Execute(true, true, false);
+        }
+    }
+}
